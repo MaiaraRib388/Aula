599d7aa306c25df9f7a6545caf855fc41844b125
MaiaraRib388 <113558275+MaiaraRib388@users.noreply.github.com>
2022-10-10 16:25:16 -0300

    Add files via upload
    JavaScript que finalmente Aprendi!

---
+++ JavaScript Will/Js/script.js
@@ -0,0 +1,98 @@
+// console.log ("Boa Tarde" +" Turma");
+
+// var parteDoDia = "Boa Tarde";
+// var idadeInstrutor = 30;
+// var alturaInstrutor = 1.82;
+
+// var parteDoDia = "Bom dia";
+// console.log(typeof parteDoDia);
+
+// var comeuAlmoço = true;
+
+// //Se eu comi tudo, ganho sobremesa
+// if(comeuAlmoço == true){
+//     console.log("Ganhou Sobremesa!!")
+// }
+
+// //Se eu não comi tudo, não ganho sobemesa
+// else{
+//     console.log("Deu bobeira, ficou sem Sobremesa.")
+// };
+
+//Senac Sabores
+//Queijo, Calabresa, Frango
+
+//queijo - grande - R$45,00
+
+//calabresa - grande - R$47,00
+
+//frango - grande - R$47,00
+
+// var saborPizza = "Calabresa";
+
+// if(saborPizza == "Queijo"){
+//     console.log("Fazendo pizza de queijo.")
+// } else if (saborPizza =="Calabresa"){
+//     console.log("Fazendo pizza de calabresa.")
+// } else {
+//     console.log("Não temos esse sabor disponível.")
+// }
+
+//Senac Sabores
+//QuatroQueijo, Calabresa, Frango
+
+//Quatroqueijo - grande - R$45,00
+
+//calabresa - grande - R$47,00
+
+//frangoRequeijão - grande - R$47,00
+
+// var saborPizza = prompt("Qual pizza tu quer?")
+// var diaSemana = prompt("Que dia é hoje?")
+// //Se o cliente escolher calabresa e o dia for sexta o cliente ganha um refri
+// if (saborPizza == "calabresa" && diaSemana == "sexta"){
+//     console.log("Fazendo pizza de Calabresa")
+//     console.log("Parebeeeens, você ganhou um refri de 600ml")
+// }
+
+
+// var saborPizza = prompt("Qual pizza tu quer?")
+// var diaSemana = prompt("Que dia é hoje?")
+// //Se o cliente escolher QuatroQueijos e o dia for sexta ou sabado ele ganha frete gratis
+// if (saborPizza == "quatro queijos" && (diaSemana == "sexta" || "sabado")){
+//     console.log("Fazendo pizza de Quatro queijos")
+//     console.log("Frete Grátis!!!")
+// }
+
+//IMC
+
+// var alturaUsuario = prompt("Informe a sua altura:");
+// var pesoUsuario = prompt("Informe o seu peso:");
+
+// var imc = pesoUsuario / (alturaUsuario * alturaUsuario)
+
+// function imcUsuario () {
+//     if (imc < 17){
+//         return "Muito abaixo do peso"
+//     }
+//     else if (imc >= 17 && imc <= 18.49){
+//         return "Abaixo do peso"
+//     }
+//     else if (imc >= 18.50 && imc <= 24.99){
+//         return "Peso normal"
+//     }
+//     else if (imc >= 25 && imc <= 29.99){
+//         return "Acima do peso"
+//     }
+//     else if (imc >= 30 && imc <= 34.99){
+//         return "Obesidade 1"
+//     }
+//     else if (imc >= 35 && imc <= 39.99){
+//         return "Obesidade 2 (SEVERA)"
+//     }
+//     else if (imc >= 40){
+//         return "Obesidade 3 (MÓRBIDA)"
+//     }
+// };
+
+// alert(imcUsuario());
