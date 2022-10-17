1c53c1491acf777add8213e59179790177b1e478
MaiaraRib388 <113558275+MaiaraRib388@users.noreply.github.com>
2022-10-17 16:55:23 -0300

    2 aula javascript will

---
+++ JavaScript Will 2/J.S/script.js
@@ -0,0 +1,71 @@
+// var nomeAluno = 'William';
+// var idadeAluno = 30;
+
+//O aluno ... tem ... anos;
+
+// alert ('O aluno ' + nomeAluno + ' tem ' + idadeAluno + ' anos.'); //concatenação
+
+// alert (`O aluno ${nomeAluno} tem ${idadeAluno} anos`)
+
+// var mensagemNova = 'O dia está LINDO para ir à praia.';
+// alert(mensagemNova.toLowerCase());
+// alert(mensagemNova.toUpperCase());
+
+// var igorFesta = 'O Igor foi à festa.';
+// var laisFesta = igorFesta.replace('Igor', 'Laís');
+
+// alert(igorFesta);
+// alert(laisFesta);
+
+// var igorFesta = 'O Igor foi à festa.';
+// var tamanho = igorFesta.length;
+
+// alert(tamanho);
+
+//José mastiga 20 vezes antes de engolir.
+
+// var nMastigadas = 1;
+
+// while(nMastigadas <= 20) {
+//     alert(`Mastigada de número ${nMastigadas} realizada`)
+//     nMastigadas++
+// }
+
+// alert('Finalmente engoliu!');
+
+// var nMastigadas = 1;
+// var tipoComida ='batata';
+
+// while(nMastigadas <= 20) {
+//     if(tipoComida === 'sopa') {
+//         console.log('é só engolir')
+//         break;
+//     }
+// console.log(`Mastigada de número ${nMastigadas} realizada`)
+//     nMastigadas++;
+// }
+
+// document.write(`
+// <h1>Lista de compras</h1>
+//     <ul>
+//         <li> Feijão </li>
+//         <li> Arroz </li>
+//         <li> Macarrão </li>
+//     </ul>
+// `);
+
+// Se o usuário digitar 1 (ele vai para casa) 
+// Se ele digitar 2 (ele vai para a balada)
+// Senão, informe ao usuário que a entrada é inválida e pergunte novamente
+// até que o valor digitado seja verdadeiro (1 ou 2).
+
+// var perguntaResposta = prompt('Digite 1 se você vai ficar em casa ou 2 se você for para a balada')
+
+// while(true) {
+//     if(perguntaResposta == 1 || perguntaResposta == 2){
+//     break
+// } else {
+//     alert('Entrada inválida')
+//     perguntaResposta = prompt('Digite 1 se você vai ficar em casa ou 2 se você for para a balada')
+// }
+// }
